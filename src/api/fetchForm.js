146124cfb54af1b906de517f3693d61da2dfa064
Sergey Gulaga <ry6ens@gmail.com>
2023-02-17 22:03:23 +0200

    fetch change

--- src/api/fetchForm.js
+++ src/api/fetchForm.js
@@ -4,13 +4,13 @@ export const fetchForm = async userData => {
   console.log(userData);
   const response = await fetch(url, {
     method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+    // mode: 'cors',
+    // cache: 'no-cache',
+    // credentials: 'omit', // include, *same-origin, omit
     headers: {
       'Content-Type': 'application/json',
     },
-    referrerPolicy: 'no-referrer',
+    // referrerPolicy: 'no-referrer',
     body: JSON.stringify(userData), // body data type must match "Content-Type" header
   });
   return response.json();
